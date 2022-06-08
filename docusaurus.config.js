a040e58e4f38c0193e4bbceca6f94f9ba6ace717
Taylor <Aylr@users.noreply.github.com>
2022-06-07 18:07:00 -0600

    * change docs url to /knowledge

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,6 +36,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: "/knowledge",
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
