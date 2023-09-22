a97d6ae9c92193a769979a3033d23032451532e7
Forrest Beckwith <100245109+tuvaforrest@users.noreply.github.com>
2023-09-22 18:16:33 -0400

    Terminology catalog updates (#142)
    * Added LOINC page, added loinc to terminology.csv, adjusted catalog table(s) to limit columns and added last updated dates for all data files
    
    * alphabetized terminology.csv entries
    updated sticky header logic for new catalog column widths
    
    * alphabetized terminology.csv correctly this time, hopefully
    
    added page for icd 9 pcs
    
    * removed 0
    
    * fixed calendar link

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,6 +1,6 @@
 export const tableHeaders = ["Column","Data Type","Description", "Terminology"];
 
-export const csvTableHeaders = ["Category","Code Set Name", "Maintainer", "Last updated (source)", "Last updated (Tuva)"
+export const csvTableHeaders = ["Code Set Name", "Maintainer", "Last updated"
 ]
 export const newTableData = [
 
